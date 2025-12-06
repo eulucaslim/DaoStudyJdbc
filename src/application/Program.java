5ee774c5a69676c443a46c11af288457e11600ec
Lucas Lima <lucaslimacnt14@gmail.com>
2025-12-05 22:34:14 -0400

    feat: implements findAll

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,7 +1,6 @@
 package application;
 
 import model.dao.DaoFactory;
-import model.dao.DepartmentDao;
 import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
@@ -14,14 +13,18 @@ public class Program {
         SellerDao sellerDao = DaoFactory.createSellerDao();
 
         System.out.println("=== TEST 1: seller findById =====");
-
         Seller seller = sellerDao.findById(3);
         System.out.println(seller);
 
         System.out.println("=== TEST 2: seller findByDepartment =====");
         Department dep = new Department(2, null);
         List<Seller> list = sellerDao.findByDepartment(dep);
-
         list.forEach(System.out::println);
+
+        System.out.println("=== TEST 3: seller findAll =====");
+        List<Seller> sellers = sellerDao.findAll();
+        sellers.forEach(System.out::println);
+
+
     }
 }
